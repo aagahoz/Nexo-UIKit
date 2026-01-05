5040e27bcdef524e6764069b94b84d99e2b61c8b
aagahoz <ozdemiragah@gmail.com>
2026-01-05 22:35:51 +0300

    Created JobApplication model

---
+++ Nexo/Model/JobApplication.swift
@@ -0,0 +1,24 @@
+//
+//  JobApplication.swift
+//  Nexo
+//
+//  Created by Agah Ozdemir on 5.01.2026.
+//
+
+import Foundation
+
+enum ApplicationStatus {
+    case applied
+    case interview
+    case offer
+    case rejected
+}
+
+struct JobApplication {
+    let id: UUID
+    var companyName: String
+    var positionTitle: String
+    var platform: String
+    var applicationDate: Date
+    var status: ApplicationStatus
+}
