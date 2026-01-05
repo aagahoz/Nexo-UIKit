f69f2ea334b32e52ebceee6a1b59b352c383b69c
aagahoz <ozdemiragah@gmail.com>
2026-01-05 21:08:40 +0300

    Project Created

---
+++ NexoUITests/NexoUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  NexoUITestsLaunchTests.swift
+//  NexoUITests
+//
+//  Created by Agah Ozdemir on 5.01.2026.
+//
+
+import XCTest
+
+final class NexoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
